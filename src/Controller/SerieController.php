808f214ee244d07dddd559eb3bdb6f7e31ca33a0
bluezelli <96504071+bluezelli@users.noreply.github.com>
2023-04-12 12:53:46 +0200

    begin form

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Genre;
+use App\Entity\Serie;
+use App\Repository\EpisodeRepository;
 use App\Repository\GenreRepository;
 use App\Repository\SerieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,9 +21,21 @@ class SerieController extends AbstractController
     }
 
     #[Route('/serie/{id}', name: 'serie')]
-    public function movies(Genre $genre, SerieRepository $serieRepository): Response
+    public function serie(Genre $genre, SerieRepository $serieRepository): Response
     {
         $series = $serieRepository->findBy(['genre' => $genre]);
         return $this->render('serie/series.html.twig', ['series' => $series]);
+    }
+
+    #[Route('/episode/{id}', name: 'episode')]
+    public function episode(Serie $serie, EpisodeRepository $episodeRepository): Response
+    {
+        $episodes = $episodeRepository->findBy(['serie' => $serie]);
+        return $this->render('serie/episode.html.twig', ['episodes' => $episodes]);
+    }
+
+    #[Route('/insert{id}', name: 'insert')]
+    public function insert(){
+
     }
 }
